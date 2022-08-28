3353308149d406951c0c26264bdae43407ba0314
stevenmontoyat01 <montoyatorressteven0@gmail.com>
2022-08-28 17:15:42 -0500

    S.M - se modificaron archivos app y index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Route,Routes} from 'react-router-dom'
 import { Home } from '../src/components/Pages/Home/Home'
+import { Inicio } from '../src/components/Pages/Inicio/Inicio'
+import { Register } from '../src/components/Pages/Register/Register'
 function App() {
   return (
-    <Home/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/inicio' element={<Inicio/>}/>
+      <Route path='/registro' element={<Register/>}></Route>
+    </Routes>
   );
 }
 
